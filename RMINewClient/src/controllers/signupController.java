65498082f486578244c8eb5eb29b244be3d3c9ca
Sama Hany <sama193754@bue.edu.eg>
2022-12-17 18:38:55 +0200

    SIGNUP

---
+++ RMINewClient/src/controllers/signupController.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controllers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JTextField;
+import rmi.FacadeInterface;
+import rmi.person;
+import rminewclient.signupForm;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class signupController {
+    signupForm sf = new signupForm();
+    Registry r;
+    person p;
+    public signupController(signupForm sf, Registry r, person p){
+        this.sf = sf;
+        this.r = r;
+        this.p = p;
+        
+        sf.getjButton1().addActionListener(new jbutton1Action(this.p));
+    }
+    class jbutton1Action implements ActionListener{
+
+        private person p;
+        
+        public jbutton1Action (person p) {
+            this.p = p;
+        }
+        
+        @Override
+         public void actionPerformed(ActionEvent ae) {
+            try{
+                FacadeInterface g = (FacadeInterface) r.lookup("Person");
+                
+                String Fname = sf.getjTextField2().getText();
+                String Lname = sf.getjTextField3().getText();
+                String age = sf.getjTextField5().getText();
+                int Age =Integer.parseInt(age);
+                String Email = sf.getjTextField6().getText();
+                String Phone = sf.getjTextField7().getText();
+                String Address = sf.getjTextField8().getText();
+                String Username = sf.getjTextField9().getText();
+                String Password = sf.getjTextField10().getText();
+                
+                g.setPerson(0, Fname, Lname, "Female",  Age, Email, Phone, Address, Username, Password);
+            
+            } catch (RemoteException ex) {
+                Logger.getLogger(signupController.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (NotBoundException ex) {
+                Logger.getLogger(signupController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }
+    }
+}
---
+++ RMINewServer/src/rmi/person.java
@@ -0,0 +1,121 @@
+package rmi;
+
+import rminewserver.*;
+
+
+public class person{
+    
+    private int ID;
+    private String Fname;
+    private String Lname;
+    private String gender;
+    private int age;
+    private String email;
+    private String phone;
+    private String address;
+    private String username;
+    private String password;
+
+    public person(){
+    }
+
+    public person(int ID, String Fname, String Lname, String gender, int age, String email, String phone, String address, String username, String password) {
+        this.ID = ID;
+        this.Fname = Fname;
+        this.Lname = Lname;
+        this.gender = gender;
+        this.age = age;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.username = username;
+        this.password = password;
+    }
+
+    public person(String phone, String address, String username, String password){
+        this.phone = phone;
+        this.address = address;
+        this.username = username;
+        this.password = password;
+    }
+    
+    public int getID(){
+        return ID;
+    }
+
+    public void setID(int ID){
+        this.ID = ID;
+    }
+
+    public String getFname(){
+        return Fname;
+    }
+
+    public void setFname(String Fname){
+        this.Fname = Fname;
+    }
+
+    public String getLname(){
+        return Lname;
+    }
+
+    public void setLname(String Lname){
+        this.Lname = Lname;
+    }
+
+    public String getGender(){
+        return gender;
+    }
+
+    public void setGender(String gender){
+        this.gender = gender;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+    
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getPhone(){
+        return phone;
+    }
+
+    public void setPhone(String phone){
+        this.phone = phone;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+
+    public void setAddress(String address){
+        this.address = address;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+}
